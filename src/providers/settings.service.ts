cdf5ae2a43d82a45cc7f87b5ca45b6a2d0f2bb8d
Davina Leong <leong.shi.yun@gmail.com>
2017-07-27 22:06:37 +0800

    Implemented form values in Settings service.

--- src/providers/settings.service.ts
+++ src/providers/settings.service.ts
@@ -1,3 +1,56 @@
+import { Injectable } from '@angular/core';
+import { Translation } from '../interfaces/translation.interface';
 import { Settings } from '../interfaces/settings.interface';
 
-import { settingsData } from '../data/settings';
+import passages from '../data/passages';
+import settings from '../data/settings';
+
+@Injectable()
+export class SettingsService {
+
+    private _settings: Settings = null;
+    get settings(): Settings {
+        return this._settings;
+    }
+    set settings(settings: Settings) {
+        this._settings = settings;
+    }
+
+    public themes(): string[] {
+        return [
+            'Dark',
+            'Light'
+        ];
+    }
+
+    public fontSizes(): string[] {
+        return [
+            'small',
+            'medium',
+            'large'
+        ];
+    }
+
+    public fontTypes(): string[] {
+        return [
+            'Sans-Serif',
+            'Serif'
+        ];
+    }
+
+    public views(): string[] {
+        return [
+            'Grid',
+            'Paragraph'
+        ]
+    }
+
+    public translationIds(): number[] {
+        let translationIds: number[] = null;
+        for (let translation of passages) {
+            translationIds.push(translation.id);
+        }
+        return translationIds;
+    }
+
+} //end SettingsService class
